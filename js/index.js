0f67288f08cad5566d0191950ed78276046f6582
Insurikai <40343703+Insurikai@users.noreply.github.com>
2019-10-01 14:44:36 -0700

    Added references to the elements i plan on changing

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+const banner = document.querySelector(".intro img");
+const body = document.querySelector("body");
+const header = document.querySelector("header");
+const navLogo = document.querySelector('.logo-heading');
+const buttonOne = document.querySelector(".destination .btn");
+const pictures = document.querySelectorAll(".img-content img");
+const pictureHeadings = document.querySelectorAll(".text-content h2");
+const signUpButtons = document.querySelectorAll(".btn");
