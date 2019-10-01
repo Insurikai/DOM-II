517618a033f9aaf8a6a20149e246cf24b8d9fb70
Insurikai <40343703+Insurikai@users.noreply.github.com>
2019-10-01 14:45:10 -0700

    Banner 'mouseenter' eventListener

--- js/index.js
+++ js/index.js
@@ -6,3 +6,8 @@ const buttonOne = document.querySelector(".destination .btn");
 const pictures = document.querySelectorAll(".img-content img");
 const pictureHeadings = document.querySelectorAll(".text-content h2");
 const signUpButtons = document.querySelectorAll(".btn");
+
+banner.addEventListener("mouseenter", function bannerSmall(e){
+    banner.style = "width:90%; margin: 0 5%;";
+    banner.removeEventListener("mouseenter", bannerSmall, false);
+}, false);
